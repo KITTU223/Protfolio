ec5cc8e493c966178347fe8cdc8edd7bcdda5a04
Krutarth Chauhan <63405029+KITTU223@users.noreply.github.com>
2021-10-16 15:44:38 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const menuBtn = document.querySelector('.menu-btn');
+const navigation = document.querySelector('.navigation');
+menuBtn.addEventListener("click", () =>{
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+//for videos
+const btns = document.querySelectorAll('.nav-btn');
+const slider = document.querySelectorAll('.video-slider');
+const contents = document.querySelectorAll('.content');
+
+var sliderNav = function(manual){
+    btns.forEach((btn) =>{
+        btn.classList.remove('active');
+    });
+    slider.forEach((slide) =>{
+        slide.classList.remove('active');
+    });
+    contents.forEach((content) =>{
+        content.classList.remove('active');
+    });
+    btns[manual].classList.add('active');
+    slider[manual].classList.add('active');
+    contents[manual].classList.add('active');
+}
+
+btns.forEach((btn, i) =>{
+    btn.addEventListener('click', () =>{
+        sliderNav(i);
+    });
+});
