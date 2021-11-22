9e56c0e32f0671e1929b1dd0b85016229a510c07
Krutarth Chauhan <63405029+KITTU223@users.noreply.github.com>
2021-11-22 16:34:40 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -30,3 +30,20 @@ btns.forEach((btn, i) =>{
         sliderNav(i);
     });
 });
+
+//skills
+
+const fill = document.querySelector('.fillpercentage');
+const skillbox = document.querySelector('.skill_container');
+
+fillbar = (e) =>{
+fill.forEach(eachfill=>{
+    let cls = eachfill.getAttribute('class');
+    if(e){
+        eachfill.setAttribute("class", cls.replace("cmt_",""));
+    }else{
+        eachfill.setAttribute("class", cls.replace("w-", "cmt_w-"));
+    }
+})
+}
+intoview(skillbox,fillbox);
